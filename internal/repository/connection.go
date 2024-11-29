b7b665d2c950628ef73a085c56a241d337320ce4
lemon <lemon307@mail.ru>
2024-11-29 20:20:40 +0700

    Implemented work with channels and blogs

--- internal/repository/connection.go
+++ internal/repository/connection.go
@@ -12,6 +12,7 @@ import (
 var DB *gorm.DB
 
 func Connect() {
+
 	host := os.Getenv("DB_HOST")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
@@ -19,6 +20,8 @@ func Connect() {
 	port := os.Getenv("DB_PORT")
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+
+	fmt.Println(dsn)
 
 	var err error
 	for i := 0; i < 10; i++ {
